afae84e2314e66e1aa4606ee4e14aaf0cea344d2
alekseyzvyagintsev <alex0236889@gmail.com>
2025-09-16 12:53:07 +0300

    Доработаны шаблон login.html и urls.py, admin.py

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,7 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='catalog:home'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'),
     path('accounts/profile/<int:pk>/', ProfileDetailView.as_view(), name='profile'),
     path("accounts/profile/update/<int:pk>/", ProfileUpdateView.as_view(), name="profile_update"),
     path("accounts/profile/delete/<int:pk>/", ProfileDeleteView.as_view(), name="profile_delete"),
