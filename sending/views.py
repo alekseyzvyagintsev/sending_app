6950c24bd591af4f20a4e56e4780198e8c372eff
alekseyzvyagintsev <alex0236889@gmail.com>
2025-09-16 13:50:18 +0300

    Добавлено представление MailingCreateView, шаблон mailing_form.html для создания рассылки и настроен urls

--- sending/views.py
+++ sending/views.py
@@ -1,8 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, CreateView
 
+from sending.forms import MailingForm
 from sending.models import Mailing, MailingAttempt, Message, MessageRecipient
 
 
@@ -21,7 +24,7 @@ class ContactView(View):
 class MailingListView(LoginRequiredMixin, ListView):
     model = Mailing
     paginate_by = 20
-    template_name = 'sending/mailing_list.hyml'
+    template_name = 'sending/mailing_list.html'
     context_object_name = 'mailings'
     permission_required = 'sending.view_mailing'
 
@@ -43,5 +46,33 @@ class MailingListView(LoginRequiredMixin, ListView):
         return context
 
 
-class MailingCreateView(LoginRequiredMixin, CreateView):
-    ...
+class MailingCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Mailing
+    form_class = MailingForm
+    template_name = "sending/mailing_form.html"
+    success_message = "Вы успешно создали новый товар!"
+    success_url = reverse_lazy("sending:mailing_list")
+    # permission_required = 'catalog.add_mailing'
+
+    def form_valid(self, form):
+        """
+        Метод вызывается, когда форма прошла валидацию.
+        Здесь мы добавляем текущего пользователя в качестве владельца рассылки .
+        """
+        # Получаем объект продукта из формы
+        obj = form.save(commit=False)
+
+        # Устанавливаем текущего пользователя в качестве владельца
+        obj.owner = self.request.user
+
+        # Сохраняем объект продукта в базе данных
+        obj.save()
+
+        # Возвращаем стандартный ответ родительского класса
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        """Добавляем получателей в контекст шаблона"""
+        context = super().get_context_data(**kwargs)
+        context["recipients"] = MessageRecipient.objects.all()
+        return context
