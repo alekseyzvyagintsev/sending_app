debe2fc124e7af4562fcbd2e8cf89701e0f2de80
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-10-20 21:27:53 +0300

    Добавлено кеширование некоторых страниц: Home; Contact; ...ListView; ...DetailView; Statistics.

--- sending/apps.py
+++ sending/apps.py
@@ -5,5 +5,3 @@ class SendingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sending'
 
-    # def ready(self):
-    #     from . import signals  # Импортируем сигналы, чтобы Django мог их обнаружить
